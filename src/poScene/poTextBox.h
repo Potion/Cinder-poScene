cca93de2df9090f775940d0eeabd601624a764ad
Stephen Varga <stevevarga@steve.local>
2014-05-15 14:28:11 -0400

    Cleaned up/adjusted a few small things

--- src/poScene/poTextBox.h
+++ src/poScene/poTextBox.h
@@ -34,19 +34,19 @@ namespace po {
         ci::Rectf getBounds();
         
         //Override ci::TextBox Functions so we can return the po::Textbox instead of base class
-        po::TextBox& size( ci::Vec2i sz ) { ci::TextBox::setSize( sz ); return *this; }
-        po::TextBox& text( const std::string &t ) { ci::TextBox::setText( t ); return *this; }
-        po::TextBox& font( const ci::Font &f ) { setFont( f ); return *this; }
-        po::TextBox& textAlignment( ci::TextBox::Alignment align ) { ci::TextBox::setAlignment( align ); return *this; }
-        po::TextBox& color( ci::ColorA color ) { ci::TextBox::setColor( color ); return *this; }
-        po::TextBox& premultiplied( bool premult = true ) { ci::TextBox::setPremultiplied( premult ); return *this; }
-        po::TextBox& ligate( bool ligateText = true ) { ci::TextBox::setLigate( ligateText ); return *this; }
+        po::TextBox& size(ci::Vec2i sz) { ci::TextBox::setSize(sz);                                        return *this; }
+        po::TextBox& text(const std::string &t) { ci::TextBox::setText(t);                                 return *this; }
+        po::TextBox& font(const ci::Font &f) { setFont(f);                                                 return *this; }
+        po::TextBox& textAlignment(ci::TextBox::Alignment align) { ci::TextBox::setAlignment( align );     return *this; }
+        po::TextBox& premultiplied(bool premult = true) { ci::TextBox::setPremultiplied( premult );        return *this; }
+        po::TextBox& ligate(bool ligateText = true) { ci::TextBox::setLigate( ligateText );                return *this; }
+        po::TextBox& color(ci::Color color) { setColor(color);                                             return *this; };
+        po::TextBox& color(float r, float g, float b) { setColor(ci::Color(r,g,b));                        return *this; };
         
     protected:
         TextBox();
         
     private:
-        ci::Color textColor, fillColor, strokeColor;
         ci::gl::TextureRef mTexture;
     };
 };
