1a52e07a843dd1d6e470c2a1a099d9650e6559dd
Vtron <steve@vargatron.com>
2014-08-31 04:53:01 -0400

    Seems to be working, need to clean up

--- src/poScene/poVideoPlayer.cpp
+++ src/poScene/poVideoPlayer.cpp
@@ -10,41 +10,5 @@
 
 
 namespace po {
-    VideoPlayerRef VideoPlayer::create() {
-        VideoPlayerRef ref = VideoPlayerRef(new VideoPlayer());
-        ref->setup();
-        return ref;
-    }
     
-    VideoPlayerRef VideoPlayer::create(ci::qtime::MovieGlRef movieRef) {
-        VideoPlayerRef ref = VideoPlayerRef(new VideoPlayer());
-        ref->setup();
-        ref->setMovieRef(movieRef);
-        return ref;
-    }
-    
-    VideoPlayer::VideoPlayer() {}
-    
-    void VideoPlayer::setup() {}
-    
-    void VideoPlayer::update()
-    {
-        ci::qtime::MovieGlRef m = mMovieRef.lock();
-    }
-    
-    ci::Rectf VideoPlayer::getBounds()
-    {
-        ci::qtime::MovieGlRef m = mMovieRef.lock();
-        if(m) return m->getBounds();
-        return ci::Rectf(0,0,0,0);
-    }
-    
-    void VideoPlayer::draw()
-    {
-        ci::qtime::MovieGlRef m = mMovieRef.lock();
-        if(m && m->getTexture()) {
-            ci::gl::color(ci::ColorA(getFillColor(), getAppliedAlpha()));
-            ci::gl::draw(m->getTexture());
-        }
-    }
 }
--- src/poScene/poVideoPlayer.h
+++ src/poScene/poVideoPlayer.h
@@ -10,36 +10,68 @@
 
 #include "poNode.h"
 
+#include "boost/any.hpp"
+
 #include "cinder/app/AppBasic.h"
 #include "cinder/gl/Texture.h"
 #include "cinder/qtime/QuickTime.h"
 
 namespace po {
-    class VideoPlayer;
-    typedef std::shared_ptr<VideoPlayer> VideoPlayerRef;
-    
+    template<class T>
     class VideoPlayer
     : public po::Node {
-    public:
-        static VideoPlayerRef create();
-        static VideoPlayerRef create(ci::qtime::MovieGlRef movieRef);
-        
-        void setup();
-        void update();
-        void draw();
-        
-        //Returns the bounds of our video (if we have one)
-        ci::Rectf getBounds();
-        
-        ci::qtime::MovieGlRef getMovieRef() { return mMovieRef.lock(); };
-        void setMovieRef(ci::qtime::MovieGlRef movieRef) { mMovieRef = movieRef; };
-        void clearMovieRef();
-        
-    protected:
-        VideoPlayer();
         
     private:
+        typedef std::shared_ptr<T> GenericMovieRef;
+        
+    public:
+        static std::shared_ptr<VideoPlayer<T> > create() {
+            std::shared_ptr<VideoPlayer<T> > ref = std::shared_ptr<VideoPlayer<T> >(new VideoPlayer());
+            ref->setup();
+            return ref;
+        }
+        
+        static std::shared_ptr<VideoPlayer<T> > create(GenericMovieRef movieRef) {
+            std::shared_ptr<VideoPlayer<T> > ref = std::shared_ptr<VideoPlayer<T> >(new VideoPlayer());
+            ref->setup();
+            ref->setMovieRef(movieRef);
+            return ref;
+        }
+        
+        void setMovieRef(GenericMovieRef movieRef)
+        {
+            mMovieRef = movieRef;
+        };
+        
+        GenericMovieRef getMovieRef() { return mMovieRef; };
+        
+        ci::Rectf getBounds()
+        {
+            GenericMovieRef m = mMovieRef.lock();
+            if(m) return m->getBounds();
+            return ci::Rectf(0,0,0,0);
+        }
+        
+    protected:
+        VideoPlayer() {}
+        
+        void setup() {}
+        void update(){}
+        
+        void draw()
+        {
+            GenericMovieRef m = mMovieRef.lock();
+            if(m && m->getTexture()) {
+                ci::gl::color(ci::ColorA(getFillColor(), getAppliedAlpha()));
+                ci::gl::draw(m->getTexture());
+            }
+        }
+        
+    private:
+        
         //Movie and texture refs
-        std::weak_ptr<ci::qtime::MovieGl> mMovieRef;
+        std::weak_ptr<T> mMovieRef;
     };
+    
+    template<class T> using VideoPlayerRef = std::shared_ptr<VideoPlayer<T> >;
 }
