684b9f516c97cdda257e10add8906c182d2515af
bruce <brucedrummond@gmail.com>
2015-05-18 17:05:48 -0400

    loading shaders from poscene resources

--- samples/Masking/include/Resources.h
+++ samples/Masking/include/Resources.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
 
-#define RES_GLSL_PO_MASK_VERT    CINDER_RESOURCE( ../resources/, poMask_vert.glsl, 135, GLSL )
-#define RES_GLSL_PO_MASK_FRAG    CINDER_RESOURCE( ../resources/, poMask_frag.glsl, 136, GLSL )
+#define PROJECT_ROOT	"../../../../"
+//#define RES_GLSL_PO_MASK_VERT    CINDER_RESOURCE( ../resources/, poMask_vert.glsl, 135, GLSL )
+//#define RES_GLSL_PO_MASK_FRAG    CINDER_RESOURCE( ../resources/, poMask_frag.glsl, 136, GLSL )
+
+#define RES_GLSL_PO_MASK_VERT    CINDER_RESOURCE( PROJECT_ROOT + "resources/poSceneResources/shaders/", poMask_vert.glsl, 135, GLSL )
+#define RES_GLSL_PO_MASK_FRAG    CINDER_RESOURCE( PROJECT_ROOT + "resources/poSceneResources/shaders/", poMask_frag.glsl, 136, GLSL )
