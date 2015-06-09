0511d24e021c22170119c783fb99d03b0c762764
Jennifer <jennifer@potiondesign.com>
2015-06-09 14:27:48 -0400

    changed alignment sample for new TextBox class

---
+++ samples/AnimationSample/src/AnimationSquare.cpp
@@ -0,0 +1,9 @@
+//
+//  AnimationSquare.cpp
+//  AnimationSample
+//
+//  Created by Jennifer on 6/9/15.
+//
+//
+
+#include "AnimationSquare.h"
---
+++ samples/AnimationSample/src/AnimationSquare.h
@@ -0,0 +1,14 @@
+//
+//  AnimationSquare.h
+//  AnimationSample
+//
+//  Created by Jennifer on 6/9/15.
+//
+//
+
+#ifndef __AnimationSample__AnimationSquare__
+#define __AnimationSample__AnimationSquare__
+
+#include <stdio.h>
+
+#endif /* defined(__AnimationSample__AnimationSquare__) */
