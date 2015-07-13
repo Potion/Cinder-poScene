52a528a2892b4f2319d783b64feeee32ae576d06
Jennifer <jennifer@potiondesign.com>
2015-07-13 17:47:41 -0400

    removed QuickTime framework

--- samples/VideoSample/src/VideoSample.h
+++ samples/VideoSample/src/VideoSample.h
@@ -3,10 +3,6 @@
 #include "poNodeContainer.h"
 #include "poVideo.h"
 
-// In order to use poVideo, you must link the QuickTime.framework.
-// In Xcode, you can add this under Targets -> BuildPhases -> Link Binary With Libraries.
-// Hit [+] at the bottom, and choose QuickTime.framework from the list.
-
 
 class VideoSample;
 typedef std::shared_ptr<VideoSample> VideoSampleRef;
