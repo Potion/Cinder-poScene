3cfd3762ab5d000357bd0ba529d9d6c61484cfd2
Cameron Browning <cameron@potiondesign.com>
2017-01-30 11:51:18 -0500

    updated to use xcpretty, which reported two linked warnings (fixed herein)

--- samples/buildAll-mac.sh
+++ samples/buildAll-mac.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# helper script for xcode samples
+# uses xcpretty for formatting output https://github.com/supermarin/xcpretty
+# install with `gem install xcpretty`
+
 echo "Ready to build all poScene examples"
 read -p "Press enter to continue"
 echo "Building all poScene examples..."
@@ -7,17 +11,17 @@ echo "Building all poScene examples..."
 # Execute each build in a subshell
 # ref: http://stackoverflow.com/questions/786376/how-do-i-run-a-program-with-a-different-working-directory-from-current-from-lin
 
-(cd AlignmentSample/xcode && exec xcodebuild)
-(cd AnimationSample/xcode && exec xcodebuild)
-(cd BoundsSample/xcode && exec xcodebuild)
-(cd DragAndDrop/xcode && exec xcodebuild)
-(cd HierarchySample/xcode && exec xcodebuild)
-(cd MaskingSample/xcode && exec xcodebuild)
-(cd MouseEventsSample/xcode && exec xcodebuild)
-(cd ShapeTextureSample/xcode && exec xcodebuild)
-(cd TextSample/xcode && exec xcodebuild)
-(cd TextSampleAdvanced/xcode && exec xcodebuild)
-(cd VideoSample/xcode && exec xcodebuild)
-(cd VideoSampleAdvanced/xcode && exec xcodebuild)
+(cd AlignmentSample/xcode && exec xcodebuild | xcpretty)
+(cd AnimationSample/xcode && exec xcodebuild | xcpretty)
+(cd BoundsSample/xcode && exec xcodebuild | xcpretty)
+(cd DragAndDrop/xcode && exec xcodebuild | xcpretty)
+(cd HierarchySample/xcode && exec xcodebuild | xcpretty)
+(cd MaskingSample/xcode && exec xcodebuild | xcpretty)
+(cd MouseEventsSample/xcode && exec xcodebuild | xcpretty)
+(cd ShapeTextureSample/xcode && exec xcodebuild | xcpretty)
+(cd TextSample/xcode && exec xcodebuild | xcpretty)
+(cd TextSampleAdvanced/xcode && exec xcodebuild | xcpretty)
+(cd VideoSample/xcode && exec xcodebuild | xcpretty)
+(cd VideoSampleAdvanced/xcode && exec xcodebuild | xcpretty)
 
 echo "...Finished building poScene examples."
