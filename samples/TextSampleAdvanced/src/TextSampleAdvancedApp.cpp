7849adf2e28b7a4b1d27b2669da68b1648936c8c
peterobbin <beijingwang@gmail.com>
2018-10-16 18:24:01 -0400

    update text sample advanced

--- samples/TextSampleAdvanced/src/TextSampleAdvancedApp.cpp
+++ samples/TextSampleAdvanced/src/TextSampleAdvancedApp.cpp
@@ -3,7 +3,7 @@
 #include "cinder/app/RendererGl.h"
 
 #include "poScene/Scene.h"
-#include "ViewController.h"
+#include "MainViewController.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -11,33 +11,33 @@ using namespace std;
 using namespace po::scene;
 
 class TextSampleAdvancedApp
-: public App 
+	: public App
 {
-  public:
-	void setup();
-	void update();
-	void draw();
-    
-	SceneRef mScene;
-	sample::ViewControllerRef mViewController;
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		SceneRef mScene;
+		sample::ViewControllerRef mViewController;
 };
 
 void TextSampleAdvancedApp::setup()
 {
 	mViewController = sample::ViewController::create();
-	mScene = Scene::create(mViewController);
+	mScene = Scene::create( mViewController );
 }
 
 void TextSampleAdvancedApp::update()
 {
-    mScene->update();
+	mScene->update();
 }
 
 void TextSampleAdvancedApp::draw()
 {
 	// clear out the window with black
 	gl::clear( Color( 0, 0, 0 ) );
-    mScene->draw();
+	mScene->draw();
 }
 
 CINDER_APP( TextSampleAdvancedApp, RendererGl )
