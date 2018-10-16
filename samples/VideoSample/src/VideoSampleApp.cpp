7447bc552f4010561086ac2c34069d1bbc03995f
peterobbin <beijingwang@gmail.com>
2018-10-16 18:26:08 -0400

    update video sample

--- samples/VideoSample/src/VideoSampleApp.cpp
+++ samples/VideoSample/src/VideoSampleApp.cpp
@@ -3,41 +3,41 @@
 #include "cinder/app/RendererGl.h"
 
 #include "poScene/Scene.h"
-#include "ViewController.h"
+#include "MainViewController.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class VideoSampleApp
-: public App
+	: public App
 {
-public:
-    void setup();
-    void update();
-    void draw();
-	
-	po::scene::SceneRef mScene;
-	sample::ViewControllerRef mViewController;
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		po::scene::SceneRef mScene;
+		sample::ViewControllerRef mViewController;
 };
 
 void VideoSampleApp::setup()
 {
-    setWindowSize(1024, 768);
+	setWindowSize( 1024, 768 );
 	mViewController = sample::ViewController::create();
-	mScene = po::scene::Scene::create(mViewController);
+	mScene = po::scene::Scene::create( mViewController );
 }
 
 void VideoSampleApp::update()
 {
-    mScene->update();
+	mScene->update();
 }
 
 void VideoSampleApp::draw()
 {
-    // clear out the window with black
-    gl::clear( Color( 0, 0, 0 ) );
-    mScene->draw();
+	// clear out the window with black
+	gl::clear( Color( 0, 0, 0 ) );
+	mScene->draw();
 }
 
 CINDER_APP( VideoSampleApp, RendererGl )
