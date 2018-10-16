49d0855eeb9b5da20f538ba82bbab00d1b7e5e1a
peterobbin <beijingwang@gmail.com>
2018-10-16 18:05:56 -0400

    updated bounds sample

--- samples/BoundsSample/src/BoundsSampleApp.cpp
+++ samples/BoundsSample/src/BoundsSampleApp.cpp
@@ -3,41 +3,41 @@
 #include "cinder/app/RendererGl.h"
 
 #include "poScene/Scene.h"
-#include "ViewController.h"
+#include "MainViewController.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class BoundsSampleApp
-: public App
+	: public App
 {
-  public:
-	void setup();
-	void update();
-	void draw();
-    
-    po::scene::SceneRef mScene;
-	sample::ViewControllerRef mViewController;
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		po::scene::SceneRef mScene;
+		sample::ViewControllerRef mViewController;
 };
 
 void BoundsSampleApp::setup()
 {
-    setWindowSize(800, 600);
+	setWindowSize( 800, 600 );
 	mViewController	= sample::ViewController::create();
-    mScene = po::scene::Scene::create(mViewController);
+	mScene = po::scene::Scene::create( mViewController );
 }
 
 void BoundsSampleApp::update()
 {
-    mScene->update();
+	mScene->update();
 }
 
 void BoundsSampleApp::draw()
 {
 	// clear out the window with black
 	gl::clear( Color( 0.0, 0.0f, 0.0 ) );
-    mScene->draw();
+	mScene->draw();
 }
 
 CINDER_APP( BoundsSampleApp, RendererGl )
